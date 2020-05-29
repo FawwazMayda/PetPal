cc2749f89accb3acd25bab5c02af296937a1727d
fawwazmayda <maydafawwaz@gmail.com>
2020-05-29 23:28:37 +0800

    Add All

---
+++ PetPal/Data/Friend+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Friend+CoreDataClass.swift
+//  PetPal
+//
+//  Created by Muhammad Fawwaz Mayda on 29/05/20.
+//  Copyright © 2020 Razeware. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class Friend: NSManagedObject {
+
+}
---
+++ PetPal/Data/Friend+CoreDataProperties.swift
@@ -0,0 +1,22 @@
+//
+//  Friend+CoreDataProperties.swift
+//  PetPal
+//
+//  Created by Muhammad Fawwaz Mayda on 29/05/20.
+//  Copyright © 2020 Razeware. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Friend {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Friend> {
+        return NSFetchRequest<Friend>(entityName: "Friend")
+    }
+
+    @NSManaged public var name: String?
+
+}
